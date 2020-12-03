2b046e6e416ef958cbdfc65df0cee79cc1b16e5a
Yannik Sood <yanniksood7@gmail.com>
2020-12-02 22:10:38 -0800

    added redux and react router

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -3,10 +3,9 @@ import { useDispatch, connect } from 'react-redux';
 import { Row, Col, Container, Button } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import { useHistory } from 'react-router-dom';
-import * as ROUTES from '../../routes/routes';
+import * as ROUTES from './routes/routes';
 import firebase from '../../firebase';
-import { receiveUser } from '../../reducers/authReducer';
-import LoginErrors from '../Errors/LoginErrors';
+import { receiveUser } from '../reducers/authReducer';
 
 const Login = ({ isAuthed }) => {
   const [credentials, setCredentials] = useState({ email: '', password: '' });
