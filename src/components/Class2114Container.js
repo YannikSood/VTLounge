3cc237feff0ffa7e82e28ab67c3f00cd24fa2ec4
Charlotte Hahn <chahn44@vt.edu>
2020-12-08 22:54:18 -0500

    flushed out classLoungesContainer

---
+++ src/components/Class2114Container.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Class2114Container.css'
+
+class Class2114Container extends React.Component {
+
+    render() {
+        return (
+            <div className = "page">
+                <div className = "headerBar">
+                    CS 2114: Data Structures and Algorithms
+                </div>
+            </div>
+
+            
+        )
+    }
+}
+
+export default Class2114Container
